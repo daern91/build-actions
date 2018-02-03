c485fc9834dbf3565275b185de4829b636a7d2c4
daern1991 <daniel_eriksson_8@hotmail.com>
2018-02-03 17:44:27 +0100

    Implement buildActions function and make test green

--- src/buildActions.js
+++ src/buildActions.js
@@ -1,5 +1,31 @@
 'use strict';
 
+const _ = require('lodash'),
+  jsondiffpatch = require('jsondiffpatch'); //TODO: Find a better library since failing dependencies etc.
+
 module.exports = (oldCustomer, newCustomer) => {
-  return 'Hello world';
+  const diff = jsondiffpatch.diff(oldCustomer, newCustomer);
+
+  console.log('The difference between old and new customer: \n', diff);
+
+  const actions = [];
+
+  for (const [field, changes] of Object.entries(diff)) {
+    const [oldValue, newValue, isDeleted] = changes;
+
+    let operation = null;
+    if (isDeleted === 0) {
+      operation = 'delete';
+    } else if (_.isNil(newValue)) {
+      operation = 'set';
+    } else if (!_.isNil(oldValue) && !_.isNil(newValue)) {
+      operation = 'change';
+    } else {
+      throw new Error('Incorrect data type');
+    }
+    // Use snake case to preserve the Object field name
+    actions.push(`${operation}${field[0].toUpperCase()}${field.substring(1)}`);
+  }
+
+  return actions;
 };
